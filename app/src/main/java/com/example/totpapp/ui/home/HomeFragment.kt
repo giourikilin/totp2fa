0f2f182aa70aad0fc40c160b669111e91595b04d
giouri_kilin <g.kilinkaridis@student.rug.nl>
2023-10-21 23:22:19 +0200

    Added list of object for accounts

--- app/src/main/java/com/example/totpapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/totpapp/ui/home/HomeFragment.kt
@@ -4,35 +4,51 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.totpapp.R
 import com.example.totpapp.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
+    private lateinit var recyclerView: RecyclerView
+    private val itemModelList = ArrayList<HomeViewModel>()
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        recyclerView = binding.listView.findViewById(R.id.listView)
+        return binding.root
+    }
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadData()
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+        recyclerView.apply {
+            recyclerView.layoutManager = LinearLayoutManager(context)
+            adapter = CustomListAdapter(itemModelList)
+            recyclerView.adapter = adapter
         }
-        return root
+    }
+
+    private fun loadData(){
+        val item1 = HomeViewModel("RUG", "giouri123", "234682", 30)
+        val item2 = HomeViewModel("RUG", "elena123", "981223", 30)
+        val item3 = HomeViewModel("RUG", "johana123", "645732", 30)
+        itemModelList.add(item1)
+        itemModelList.add(item2)
+        itemModelList.add(item3)
     }
 
     override fun onDestroyView() {
