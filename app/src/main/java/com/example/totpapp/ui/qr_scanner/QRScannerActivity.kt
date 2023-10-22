7828408a090e12111fc58d5e401237e357aaa2a3
johanna58 <johanna.lipka@googlemail.com>
2023-10-22 15:59:08 +0200

    added open qr-scanner

--- app/src/main/java/com/example/totpapp/ui/qr_scanner/QRScannerActivity.kt
+++ app/src/main/java/com/example/totpapp/ui/qr_scanner/QRScannerActivity.kt
@@ -1,24 +1,33 @@
 package com.example.totpapp.ui.qr_scanner
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.example.totpapp.MainActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.totpapp.R
+import com.example.totpapp.databinding.ActivityQrScannerBinding
+import com.google.zxing.integration.android.IntentIntegrator
 
 class QRScannerActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr_scanner)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        if (ContextCompat.checkSelfPermission(this@QRScannerActivity, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this@QRScannerActivity, arrayOf(android.Manifest.permission.CAMERA), 123)
+        }
+
+        var scanner = IntentIntegrator(this@QRScannerActivity)
+        scanner.setPrompt("Scan qr code")
+        scanner.setOrientationLocked(true);
+        scanner.initiateScan()
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        var intent = Intent(this@QRScannerActivity, MainActivity::class.java)
-        startActivity(intent)
-        return super.onOptionsItemSelected(item)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
+
 }
