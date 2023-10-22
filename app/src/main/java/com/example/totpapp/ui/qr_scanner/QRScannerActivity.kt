bb0e830673a360d39ade8c57a01e9aae138a935f
johanna58 <johanna.lipka@googlemail.com>
2023-10-22 17:57:06 +0200

    extracting secret and issuer

--- app/src/main/java/com/example/totpapp/ui/qr_scanner/QRScannerActivity.kt
+++ app/src/main/java/com/example/totpapp/ui/qr_scanner/QRScannerActivity.kt
@@ -11,8 +11,9 @@ import androidx.core.content.ContextCompat
 import com.example.totpapp.R
 import com.example.totpapp.databinding.ActivityQrScannerBinding
 import com.google.zxing.integration.android.IntentIntegrator
-import org.json.JSONException
-import org.json.JSONObject
+import java.net.URL
+import java.net.URLDecoder
+
 
 class QRScannerActivity: AppCompatActivity() {
 
@@ -41,12 +42,38 @@ class QRScannerActivity: AppCompatActivity() {
                 Toast.makeText(this, getString(R.string.result_not_found), Toast.LENGTH_LONG).show()
             } else {
                 // If QRCode contains data.
+                verifyFormat(result.contents)
                 findViewById<TextView>(R.id.qr_data).setText(result.contents)
+
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
-
     }
 
+    fun verifyFormat(url: String) : Boolean{
+        try {
+            val query = url.removePrefix("otpauth://totp/")
+            val secret = ("secret=[A-Z, 0-9, a-z]*".toRegex()).find(query)!!
+            val issuer = ("issuer=[A-Z, 0-9, a-z]*".toRegex()).find(query)!!
+            //Toast.makeText(this, issuer.value + "", Toast.LENGTH_LONG).show()
+            /*val reg = (issuer.value + ":.*?").toRegex()
+            if (reg!= null){
+                val accountName = (reg).find(query)!!
+                Toast.makeText(this, "not null", Toast.LENGTH_LONG).show()
+            }
+
+            val accountName : MatchResult
+            if (issuer != null){
+                 accountName = ((issuer.value + "[:,%3A].?").toRegex()).find(query)!!
+            } else {
+                accountName = (".?").toRegex().find(query)!!
+            }*/
+            Toast.makeText(this, secret.value + " " + issuer.value, Toast.LENGTH_LONG).show()
+        }catch (e: Exception){
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+        }
+        //Toast.makeText(this, result.toString(), Toast.LENGTH_LONG).show()
+        return true
+    }
 }
