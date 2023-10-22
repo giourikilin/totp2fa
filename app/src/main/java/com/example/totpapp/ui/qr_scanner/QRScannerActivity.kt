56929ac22eb307dc849bff0a6f9117a44dc2f6b4
johanna58 <johanna.lipka@googlemail.com>
2023-10-22 20:41:59 +0200

    successfully extracting all fields

--- app/src/main/java/com/example/totpapp/ui/qr_scanner/QRScannerActivity.kt
+++ app/src/main/java/com/example/totpapp/ui/qr_scanner/QRScannerActivity.kt
@@ -11,8 +11,6 @@ import androidx.core.content.ContextCompat
 import com.example.totpapp.R
 import com.example.totpapp.databinding.ActivityQrScannerBinding
 import com.google.zxing.integration.android.IntentIntegrator
-import java.net.URL
-import java.net.URLDecoder
 
 
 class QRScannerActivity: AppCompatActivity() {
@@ -42,38 +40,28 @@ class QRScannerActivity: AppCompatActivity() {
                 Toast.makeText(this, getString(R.string.result_not_found), Toast.LENGTH_LONG).show()
             } else {
                 // If QRCode contains data.
-                verifyFormat(result.contents)
-                findViewById<TextView>(R.id.qr_data).setText(result.contents)
-
+                if (!extractValues(result.contents)){
+                    findViewById<TextView>(R.id.qr_data).setText(R.string.result_not_found)
+                }
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
-    fun verifyFormat(url: String) : Boolean{
+    fun extractValues(url: String) : Boolean{
         try {
             val query = url.removePrefix("otpauth://totp/")
             val secret = ("secret=[A-Z, 0-9, a-z]*".toRegex()).find(query)!!
             val issuer = ("issuer=[A-Z, 0-9, a-z]*".toRegex()).find(query)!!
-            //Toast.makeText(this, issuer.value + "", Toast.LENGTH_LONG).show()
-            /*val reg = (issuer.value + ":.*?").toRegex()
-            if (reg!= null){
-                val accountName = (reg).find(query)!!
-                Toast.makeText(this, "not null", Toast.LENGTH_LONG).show()
-            }
-
-            val accountName : MatchResult
-            if (issuer != null){
-                 accountName = ((issuer.value + "[:,%3A].?").toRegex()).find(query)!!
-            } else {
-                accountName = (".?").toRegex().find(query)!!
-            }*/
-            Toast.makeText(this, secret.value + " " + issuer.value, Toast.LENGTH_LONG).show()
+            val label = query.substringBefore("?")
+            findViewById<TextView>(R.id.label_text).setText(label)
+            findViewById<TextView>(R.id.issuer_text).setText(issuer.value)
+            findViewById<TextView>(R.id.secret_text).setText(secret.value)
         }catch (e: Exception){
-            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "This is not a valid QR code", Toast.LENGTH_LONG).show()
+            return false
         }
-        //Toast.makeText(this, result.toString(), Toast.LENGTH_LONG).show()
         return true
     }
 }
